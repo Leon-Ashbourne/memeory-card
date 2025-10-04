bc66f191906ccd148074650638e087337e3cd0cb
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2025-10-04 23:08:17 +0530

    Add the basic elements to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,118 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState, useEffect} from 'react';
+import './game.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Test your memory with this game</h1>
+      <p>Get points by clicking on an image but don't click on any more than once!</p>
+    </div>
+    <GameBody />
     </>
   )
 }
 
-export default App
+
+
+async function fetchImages() {
+
+  // fetches data from api
+}
+
+
+
+function GameBody() {
+  const [userScore, setUserScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [cards, setCards] = useState([]);
+
+  function handleGameOver() {
+    if(userScore > bestScore) setBestScore(userScore);
+    setUserScore(0);
+  }
+  function handleScoreIncrease() {
+    setUserScore(userScore + 1);
+  }
+
+  useEffect(() =>  {
+    let ignore = false;
+
+    if(!ignore) {
+      async function loadData() {
+        // updating the list
+      }
+
+      loadData();
+    }
+
+    return () => ignore = true;
+
+  }, [])
+
+  return (
+    <>
+      <div>
+        <span>Current Streak: {userScore}</span>
+        <span>Best Score: {bestScore}</span>
+      </div>
+      <Main 
+      onGameOver={handleGameOver}
+      onUserClick={handleScoreIncrease}
+      initialCards={cards}
+      />
+    </>
+  )
+}
+
+
+function Main({initialCards}) {
+  const [cardsList, setCardsList] = useState(initialCards);
+
+  function handleUserClick() {
+    setCardsList(randomizeList(cardsList));
+    console.log("clicked");
+  }
+
+  return (
+    <main>
+      { cardsList.length > 0 ? 
+        cardsList.map((card) => {
+          return <PlayerCard 
+          cardName={card.name}
+          key={card.id}
+          imgUrl={card.img}
+          onUserClick={handleUserClick}
+          />
+        }) : 
+        <div>Loading......</div>
+      }
+    </main>
+  )
+}
+
+function PlayerCard({cardName, onUserClick, imgUrl}) {
+  return (
+    <div onClick={onUserClick}>
+      <img src={imgUrl} alt={img}/>
+      <span id={cardName}>{cardName}</span>
+    </div>
+  )
+}
+
+
+function randomizeList(list) {
+  let length = 0;
+  const result = [...list];
+
+  while(length < list.length/2) {
+    let index = (length*31+length) % list.length;
+
+    const temp = result[length];
+    result[length] = result[index];
+    result[index] = temp;
+    length++; 
+  }
+
+  return result;
+}
